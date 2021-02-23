338e3210902f510c414450a8e25c43647d392ca0
Daniil Sunyaev <dasforrum@gmail.com>
2021-02-23 10:08:30 +0300

    feat(i10): verify api env vars before launching app

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use actix_web::{App, HttpServer};
 use actix_web::dev::Server;
 use dotenv::dotenv;
 use std::net::TcpListener;
+use std::env;
 
 mod weather_aggregator;
 mod handlers;
@@ -13,6 +14,7 @@ pub struct WeatherReport {
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     dotenv().ok();
+    verify_env_vars();
 
     let server = HttpServer::new(|| {
         App::new()
@@ -23,4 +25,9 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     .run();
 
     Ok(server)
+}
+
+fn verify_env_vars() {
+    env::var("OPEN_WEATHER_APPID").expect("OPEN_WEATHER_APPID is not specified");
+    env::var("WEATHERBIT_API_KEY").expect("WEATHERBIT_API_KEY is not specified");
 }
